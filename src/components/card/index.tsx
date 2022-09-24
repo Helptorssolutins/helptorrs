8482ad5df756c68ff0e4098cb7e3006d2bf3d948
daveclinton <clintondavid46@gmail.com>
2022-09-24 11:34:29 +0300

    ft: Add navbar

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ProductCard() {
-  return <div>ProductCard</div>;
+  return <div></div>;
 }
 
 export default ProductCard;
--- src/components/navsection/index.tsx
+++ src/components/navsection/index.tsx
@@ -1,18 +1,40 @@
-import { Avatar, Box, Button, Flex, Input, Link } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  Link,
+} from "@chakra-ui/react";
 import React from "react";
 
 function Navbar() {
   return (
-    <Flex>
-      <Link></Link>
-      <Link></Link>
-      <Input variant="filled" placeholder="Filled" />
-      <Link></Link>
+    <Flex justifyContent="space-between" pos="sticky" mt="20px">
       <Box>
-        <Button></Button>
-        <Button></Button>
-        <Avatar name="" src="" />
+        <Heading>Helptors</Heading>
       </Box>
+      <Box>
+        <Link>Sell on Helptors</Link>
+      </Box>
+      <Box>
+        <Button>Register</Button>
+      </Box>
+      <Box w="476px">
+        <Input
+          variant="filled"
+          placeholder="useless items on white background"
+        />
+      </Box>
+      <Box>
+        {" "}
+        <Link>Consumer Electronics</Link>
+      </Box>
+
+      <Button>Sign in</Button>
+      <Button>My Cart</Button>
+      <Avatar name="" src="" />
     </Flex>
   );
 }

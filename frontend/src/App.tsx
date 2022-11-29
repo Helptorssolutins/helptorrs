d0b6a9fad4d56c6b328eff1152e9785c75d7b5bf
daveclinton <clintondavid46@gmail.com>
2022-11-29 21:47:17 +0300

    fix: Remove unnecessary files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,4 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import SidebarWithNavbar from "./components/navsection";
-import ProductCard from "./components/card";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <SidebarWithNavbar children={undefined} />
-    <ProductCard />
-  </ChakraProvider>
-);
+export const App = () => <ChakraProvider theme={theme}>App</ChakraProvider>;
